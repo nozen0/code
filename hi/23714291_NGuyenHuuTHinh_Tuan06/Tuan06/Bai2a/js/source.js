2d91d70220ad39cffc86b55caa08d154bef2e712
nozen0 <23714291.thinh@student.iuh.edu.vn>
2025-05-10 20:45:26 +0700

    Delete hi directory

--- hi/23714291_NGuyenHuuTHinh_Tuan06/Tuan06/Bai2a/js/source.js
+++
@@ -1,7 +0,0 @@
-function dangky()
-{
-    var hoten = document.getElementById("ten").value;
-    var pass = document.getElementById("pass").value;
-    localStorage.setItem('t', hoten);
-    localStorage.setItem('p', pass);
-}
